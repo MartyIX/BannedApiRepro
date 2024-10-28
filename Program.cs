ee700c9624154a645af55dcccaf684bac792e7a4
MartyIX <203266+MartyIX@users.noreply.github.com>
2024-10-28 21:16:44 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.CodeAnalysis.CSharp.BannedApiAnalyzers;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        CSharpSymbolIsBannedAnalyzer analyzer = new();
+    }
+}
